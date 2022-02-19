3b4250d3f64abcaabfa868d6bc9fbddcf88ff4bc
username <2680242753@qq.com>
2022-02-19 16:09:41 +0800

    Hot-fix Commit

--- src/main/com/weiwei/GitTest.java
+++ src/main/com/weiwei/GitTest.java
@@ -4,8 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("1111");
         System.out.println("1111");
-        System.out.println("1111");
-        System.out.println("1111");
         System.out.println("2222");
+        System.out.println("hot-fix");
     }
 }
