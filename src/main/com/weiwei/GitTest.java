bc8a90699f3f9f9f18cfb63b7c649d5d1d2d3e24
username <2680242753@qq.com>
2022-02-19 16:28:13 +0800

    master1

--- src/main/com/weiwei/GitTest.java
+++ src/main/com/weiwei/GitTest.java
@@ -3,11 +3,7 @@ package main.com.weiwei;
 public class GitTest {
     public static void main(String[] args) {
 
-        System.out.println("hot-fix");
-        System.out.println("master");
-        System.out.println("hot-fix2");
-        System.out.println("push test");
-        System.out.println("master2");
+        System.out.println("master1");
 
     }
 }
